e5a48c849eca34b66295a688d47fc64bbd1a6c3e
jyesberg <joshua.yesberg@act.gov.au>
2024-02-19 09:51:16 +1100

    Added error catch for negative numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -6,6 +6,9 @@
 
 const sumAll = function(num1, num2) {
     sum = 0;
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
     for (i = num1; i <= num2; i++) {
         sum += i;
     }
