72974f97a00532f306199e0f89b1856a9ee4b3ba
jyesberg <joshua.yesberg@act.gov.au>
2024-02-18 20:22:29 +1100

    Completed first hello world, repeat string, reverse string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let stringReturn = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (i=0; i < num; i++) {
+        stringReturn += str;
+    }
+    return stringReturn;
 };
 
 // Do not edit below this line
