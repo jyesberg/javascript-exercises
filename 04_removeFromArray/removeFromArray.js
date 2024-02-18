2486f0616e524db0c8c671f9d7e1b6135ade684f
jyesberg <joshua.yesberg@act.gov.au>
2024-02-19 09:38:41 +1100

    reversed the loop to fix the issue with the index spot changing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,7 @@
 const removeFromArray = function(arr) {
-    console.log(arguments.length);
     for (x = 1; x < arguments.length; x++) {
-        // console.log(arguments[x]);
-        console.log('NEW X STARTING')
-        for (i=0; i < arr.length; i++) {
-            console.log(`arr[i] = ${arr[i]}`);
-            console.log(`arguments x = ${arguments[x]}`);
+        for (i = arr.length; i > -1; i--) {
             if (arr[i] === arguments[x]) {
-                console.log(`REMOVING ${arr[i]}`);
-                console.log(`REMOVING ${arguments[x]}`);
                 arr.splice([i], 1);
             }
         }
@@ -16,7 +9,7 @@ const removeFromArray = function(arr) {
     return arr;
 };
 
-removeFromArray([1, 2, 2, 2, 'a', 3, 4], 'a', 2, 3, 4)
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
 
 // Do not edit below this line
 module.exports = removeFromArray;
