e3a901800c8879f593cfa77c71232e8a3e5e353d
jyesberg <joshua.yesberg@act.gov.au>
2024-02-19 09:48:51 +1100

    Added pseudo and basic function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+// create a function that accepts 2 parameters
+// declare variable sum
+// Determine which number is lowest, then loop from that number to the higher number
+// Inside the loop, sum += i
+// outside the loop, return sum
 
+const sumAll = function(num1, num2) {
+    sum = 0;
+    for (i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+sumAll(1, 13);
 
 // Do not edit below this line
 module.exports = sumAll;
