c968f6e83bd84748732429ba25ef40d043e5b3f2
jyesberg <joshua.yesberg@act.gov.au>
2024-02-19 09:56:03 +1100

    Added error catch for non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -8,6 +8,8 @@ const sumAll = function(num1, num2) {
     sum = 0;
     if (num1 < 0 || num2 < 0) {
         return 'ERROR';
+    } else if (typeof num1 != "number" || typeof num2 != "number") {
+        return 'ERROR';
     }
     for (i = num1; i <= num2; i++) {
         sum += i;
