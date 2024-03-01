c5d08b1a98ddffbd3397c2bf7a204ae90ec7ee5e
jyesberg <joshua.yesberg@act.gov.au>
2024-03-01 20:27:19 +1100

    Completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+let palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    let stringReverse = str.split("").reverse().join('');
+    if (stringReverse === str) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
