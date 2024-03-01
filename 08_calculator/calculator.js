19295607bf265f07a8869dcaaa2790b052a3c716
jyesberg <joshua.yesberg@act.gov.au>
2024-03-01 20:15:46 +1100

    finished calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  // function sumAgain(total, currentItem) {
+  //   return total + currentItem;
+  // }
+  // return arr.reduce(sumAgain, 0);
+  return arr.reduce((total, currentItem) => total + currentItem, 0);  
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentItem) => total * currentItem, 1); 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+  for (let i=1; i <= num; i++) {
+    sum *= i;
+  }
+  return sum;	
 };
 
 // Do not edit below this line
