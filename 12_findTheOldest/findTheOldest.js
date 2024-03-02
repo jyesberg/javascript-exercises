ea30abbc962566c429a8ff07a247f54d7b41fee8
jyesberg <joshua.yesberg@act.gov.au>
2024-03-02 14:08:04 +1100

    Completed findTheOldest with a different method than in the solution.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear()
+    let newPeople = people;
+    newPeople = people.sort(function(a, b) {
+        if (a.yearOfDeath === undefined ) {
+            a.yearOfDeath = currentYear;
+        }
+        if (b.yearOfDeath === undefined ) {
+            b.yearOfDeath = currentYear;
+        }
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    })
+    return newPeople[0]
 };
 
 // Do not edit below this line
